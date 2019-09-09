e85df4c61f7106fc9ad8fb831240438cae379dd3
aldy-deptech <aldy@deptechdigital.com>
2019-09-09 18:17:26 +0700

    aldy 9 sept 2019

---
+++ GoWalls/Controller/home/home.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using System.Threading.Tasks;
+
+namespace GoWalls
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class Home : AppCompatActivity
+    {
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.home);
+        }
+    }
+}
---
+++ GoWalls/Controller/intro/Intro.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Sample;
+
+namespace GoWalls
+{
+    [Activity(Theme = "@style/FullscreenTheme")]
+    public class Intro : AppIntro.AppIntro
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            AddSlide(SampleSlide.NewInstance(Resource.Layout.intro));
+            AddSlide(SampleSlide.NewInstance(Resource.Layout.intro2));
+            AddSlide(SampleSlide.NewInstance(Resource.Layout.intro3));
+
+            SetBarColor(Color.ParseColor("#ff0057"));
+            SetSeparatorColor(Color.ParseColor("#ff0057"));
+        }
+
+        public void GetStarted(View v)
+        {
+            StartActivity(typeof(Home));
+        }
+
+        public override void OnDonePressed()
+        {
+            base.OnDonePressed();
+
+            StartActivity(typeof(Home));
+        }
+
+        public override void OnSkipPressed()
+        {
+            base.OnSkipPressed();
+            StartActivity(typeof(Home));
+            Toast.MakeText(ApplicationContext, Resource.String.skip, ToastLength.Short).Show();
+        }
+    }
+}
